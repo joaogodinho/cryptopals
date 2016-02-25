d1e76bb3a886a9449ae1dfbb6cbc32080648bcf3
joaogodinho <joaocfgodinho@gmail.com>
2016-02-25 18:37:54 +0000

    Challenge 1 complete

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -80,4 +80,8 @@ def test_hexToBase64():
 
 if __name__ == '__main__':
     test_hexToBase64()
-    print(hexToBase64(binascii.unhexlify("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))
+    original = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    expected = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    result = hexToBase64(binascii.unhexlify(original))
+    assert result == expected
+    print(result)
